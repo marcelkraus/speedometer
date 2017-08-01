f28b2ffa25cc1bccbec07d31b61554dbf1bd8525
Marcel Kraus <mail@marcelkraus.de>
2017-08-01 16:10:36 +0200

    Add speed label to storyboard and `ViewController`

--- Speedometer/ViewController.swift
+++ Speedometer/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var speedLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
