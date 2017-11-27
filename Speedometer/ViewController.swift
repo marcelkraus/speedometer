22a70450a55106633c0633f2ed00aa414f4fb088
Marcel Kraus <mail@marcelkraus.de>
2017-11-27 16:23:21 +0100

    Add concept of themes with dark and light theme

--- Speedometer/ViewController.swift
+++ Speedometer/ViewController.swift
@@ -6,12 +6,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var speedLabel: UILabel!
     @IBOutlet weak var unitLabel: UILabel!
 
-    let locationManager = CLLocationManager()
+    private var theme = Theme.light
+    private let locationManager = CLLocationManager()
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return theme == .dark ? .lightContent : .default
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.initLocationManager()
+        initGestureRecognizer()
+        initLocationManager()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -22,6 +28,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
 private extension ViewController {
 
+    enum Theme {
+        case dark, light
+    }
+
     func initLocationManager() {
         locationManager.requestWhenInUseAuthorization()
 
@@ -31,11 +41,35 @@ private extension ViewController {
         locationManager.startUpdatingLocation()
     }
 
+    func initGestureRecognizer() {
+        let recognizer = UISwipeGestureRecognizer(target: self, action: #selector(toggleTheme))
+        recognizer.direction = [.left, .right]
+        recognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(recognizer)
+    }
+
     func updateDisplay(unit: Unit, speed: Double) {
         let speed = speed > 1.0 ? speed * unit.factor : 0
 
         unitLabel.text = unit.rawValue
         speedLabel.text = String(format: "%03.0f", speed)
+    }
+
+    @objc func toggleTheme() {
+        theme = theme == .dark ? .light : .dark
+
+        switch theme {
+        case .dark:
+            view.backgroundColor = UIColor.black
+            speedLabel.textColor = UIColor.white
+            unitLabel.textColor = UIColor.white
+        case .light:
+            view.backgroundColor = UIColor.white
+            speedLabel.textColor = UIColor.black
+            unitLabel.textColor = UIColor.black
+        }
+
+        setNeedsStatusBarAppearanceUpdate()
     }
 
 }
