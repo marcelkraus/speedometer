51805d18156d2a69515e24cf727b35e1d7cc7502
Marcel Kraus <mail@marcelkraus.de>
2017-11-27 14:48:11 +0100

    Add unit label with updated constraints to storyboard

--- Speedometer/ViewController.swift
+++ Speedometer/ViewController.swift
@@ -4,15 +4,18 @@ import UIKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
 
     let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.initLocationManager()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        unitLabel.text = Unit.kilometersPerHour.rawValue
         speedLabel.text = speed(locationManager.location!.speed, inUnit: Unit.kilometersPerHour)
     }
 
