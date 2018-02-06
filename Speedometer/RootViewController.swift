895fb6ee4f070ae44732e45a7028c9717aa4f2b1
Marcel Kraus <mail@marcelkraus.de>
2018-02-06 22:43:02 +0100

    Implement `CLLocationManagerDelegate` in `RootViewController`

--- Speedometer/RootViewController.swift
+++ Speedometer/RootViewController.swift
@@ -4,6 +4,12 @@ import UIKit
 class RootViewController: UIViewController {
     private let locationManager = CLLocationManager()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager.delegate = self
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         guard CLLocationManager.authorizationStatus() != .notDetermined else {
             return transition(to: AuthorizationViewController(locationManager: locationManager, heading: "Welcome to Speedometer!", message: "This apps needs access to your location data to show your current speed.\n\nYour location data will only be used when you use this app and will never be shared.\n\nPlease press the button below and confirm access within the popup."))
@@ -13,8 +19,20 @@ class RootViewController: UIViewController {
             self.chooseViewController()
         }
     }
+}
 
-    private func chooseViewController() {
+// MARK: - CLLocationManagerDelegate
+
+extension RootViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.chooseViewController()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension RootViewController {
+    func chooseViewController() {
         switch CLLocationManager.authorizationStatus()  {
         case .restricted:
             self.transition(to: MessageViewController(heading: "Houston, we've had a problem here!", message: "Usage of this app is not possible due to restrictions of Location Services.\n\nPlease remove any restrictions in settings:\n\nSettings > General > Restrictions > Location Services\n\nor contact your administrator."))
