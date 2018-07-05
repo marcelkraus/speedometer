81ed55b8004bdcb41e34c9a1df5e666f519a4af3
Marcel Kraus <mail@marcelkraus.de>
2018-07-05 16:09:31 +0200

    Make default unit dependent of usage of metric system

--- Speedometer/Supporting Files/AppDelegate.swift
+++ Speedometer/Supporting Files/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UserDefaults.standard.register(defaults: [
             Configuration.appStartCounterKey: 0,
-            Configuration.currentUnitDefaultsKey: Unit.kilometersPerHour.rawValue,
+            Configuration.currentUnitDefaultsKey: Locale.current.usesMetricSystem ? Unit.kilometersPerHour.rawValue : Unit.milesPerHour.rawValue,
             Configuration.currentSpeedLimitDefaultsKey: "0"
         ])
 
