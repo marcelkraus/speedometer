6575cfee88e7ba463d8fbfbd0009bb176e46303f
Marcel Kraus <mail@marcelkraus.de>
2018-11-16 14:47:50 +0100

    Add subviews to `UnitSelectionViewController`

--- Speedometer/Components/UnitSelectionViewController.swift
+++ Speedometer/Components/UnitSelectionViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 
 class UnitSelectionViewController: UIViewController {
-
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var informationButton: UIButton!
 }
