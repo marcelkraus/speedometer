5f5dffd51005d9d423baead720e6d3000cd6f497
Marcel Kraus <mail@marcelkraus.de>
2018-11-16 14:42:19 +0100

    Rename `BottomBarViewController` to `UnitSelectionViewController`

--- Speedometer/Screens/LoadingViewController.swift
+++ Speedometer/Screens/LoadingViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
-    @IBOutlet weak var bottomBarView: UIView!
+    @IBOutlet weak var unitSelectionView: UIView!
     @IBOutlet weak var loadingIndicatorView: UIView!
 
     override func viewDidLoad() {
@@ -13,10 +13,10 @@ class LoadingViewController: UIViewController {
 
 private extension LoadingViewController {
     func setupContainerViewControllers() {
-        let bottomBarViewController = BottomBarViewController()
-        addChild(bottomBarViewController)
-        bottomBarView.addSubview(bottomBarViewController.view)
-        bottomBarViewController.didMove(toParent: self)
+        let unitSelectionViewController = UnitSelectionViewController()
+        addChild(unitSelectionViewController)
+        unitSelectionView.addSubview(unitSelectionViewController.view)
+        unitSelectionViewController.didMove(toParent: self)
 
         let loadingIndicatorViewController = LoadingIndicatorViewController()
         addChild(loadingIndicatorViewController)
