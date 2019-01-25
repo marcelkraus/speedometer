062b8ce622d49be7dbef3e5cae5bd00337497651
Marcel Kraus <git@marcelkraus.de>
2019-01-25 15:20:49 +0100

    Update styling of onboarding screen

---
+++ Speedometer/Views/SeparatorViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class SeparatorViewController: UIViewController {
+    private lazy var separatorView: UIView = {
+        let separatorView = UIView()
+        separatorView.backgroundColor = UIColor(red: 0.012, green: 0.569, blue: 0.576, alpha: 0.75)
+        separatorView.layer.cornerRadius = 5.0
+
+        return separatorView
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupSeparatorView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension SeparatorViewController {
+    func setupSeparatorView() {
+        view.addSubview(separatorView)
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            separatorView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            separatorView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            separatorView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            separatorView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            ])
+    }
+}
