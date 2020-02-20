a809238d3cb9c802cc80416b1129f06f15bec26d
Marcel Kraus <git@marcelkraus.de>
2020-02-20 18:34:58 +0100

    Simplify `ParagraphViewController`

--- Speedometer/Views/Components/ParagraphViewController.swift
+++ Speedometer/Views/Components/ParagraphViewController.swift
@@ -2,31 +2,32 @@ import UIKit
 
 class ParagraphViewController: UIViewController {
     private lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
+        let stackView = UIStackView(arrangedSubviews: [headingLabel, textLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .fill
         stackView.spacing = 20.0
 
         return stackView
     }()
 
     private lazy var headingLabel: UILabel = {
-        let label = UILabel()
-        label.font = .heading
-        label.textColor = .branding
-        label.numberOfLines = 2
+        let headingLabel = UILabel()
+        headingLabel.translatesAutoresizingMaskIntoConstraints = false
+        headingLabel.font = .heading
+        headingLabel.textColor = .branding
+        headingLabel.numberOfLines = 2
 
-        return label
+        return headingLabel
     }()
 
     private lazy var textLabel: UILabel = {
-        let label = UILabel()
-        label.font = .text
-        label.textColor = .text
-        label.numberOfLines = 0
+        let textLabel = UILabel()
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        textLabel.font = .text
+        textLabel.textColor = .text
+        textLabel.numberOfLines = 0
 
-        return label
+        return textLabel
     }()
 
     init(heading: String, text: String) {
@@ -34,19 +35,28 @@ class ParagraphViewController: UIViewController {
 
         self.headingLabel.text = heading
         self.textLabel.text = text
-
-        setupStackView()
     }
 
     init(messageType: MessageType) {
         super.init(nibName: nil, bundle: nil)
 
         setupFromMessage(ofType: messageType)
-        setupStackView()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
 
@@ -63,21 +73,5 @@ private extension ParagraphViewController {
             headingLabel.text = "ParagraphViewController.Onboarding.Heading".localized
             textLabel.text = "ParagraphViewController.Onboarding.Text".localized
         }
-    }
-}
-
-private extension ParagraphViewController {
-    func setupStackView() {
-        stackView.addArrangedSubview(headingLabel)
-        stackView.addArrangedSubview(textLabel)
-
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-            ])
     }
 }
