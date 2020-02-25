dfa55cceaee3aeb8a408337b3c15656329b2b9ee
Marcel Kraus <git@marcelkraus.de>
2020-02-25 18:12:52 +0100

    Simplify `MessageViewController`

--- Speedometer/Views/Screens/MessageViewController.swift
+++ Speedometer/Views/Screens/MessageViewController.swift
@@ -1,15 +1,27 @@
 import UIKit
 
 class MessageViewController: UIViewController {
-    var messageType: Message
-
-    private var paragraphViewController: ParagraphViewController!
+    private var messageType: Message
 
     private lazy var separatorView: SeparatorView = {
         let separatorView = SeparatorView()
         separatorView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(separatorView)
+        NSLayoutConstraint.activate([
+            separatorView.heightAnchor.constraint(equalToConstant: 20.0),
+            separatorView.widthAnchor.constraint(equalToConstant: 170.0),
+            separatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -30.0),
+            separatorView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40.0)
+        ])
 
         return separatorView
+    }()
+
+    private lazy var paragraphViewController: ParagraphViewController! = {
+        let paragraphViewController = ParagraphViewController(heading: messageType.heading, text: messageType.text)
+        paragraphViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        return paragraphViewController
     }()
 
     init(messageType: Message) {
@@ -25,34 +37,13 @@ class MessageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(separatorView)
-        NSLayoutConstraint.activate([
-            separatorView.heightAnchor.constraint(equalToConstant: 20.0),
-            separatorView.widthAnchor.constraint(equalToConstant: 170.0),
-            separatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -30.0),
-            separatorView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40.0)
-        ])
-
-        setupParagraphView()
-    }
-}
-
-// MARK: - UI Setup
-
-private extension MessageViewController {
-    func setupParagraphView() {
-        paragraphViewController = ParagraphViewController(heading: messageType.heading, text: messageType.text)
         addChild(paragraphViewController)
-
-        let paragraphView = paragraphViewController.view!
-        view.addSubview(paragraphView)
-
-        paragraphView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(paragraphViewController.view)
         NSLayoutConstraint.activate([
-            paragraphView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40.0),
-            paragraphView.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 40.0),
-            paragraphView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40.0),
-            ])
+            paragraphViewController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40.0),
+            paragraphViewController.view.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: 40.0),
+            paragraphViewController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40.0),
+        ])
         paragraphViewController.didMove(toParent: self)
     }
 }
