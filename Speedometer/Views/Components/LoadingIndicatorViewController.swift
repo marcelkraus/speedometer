0120ebdbece300aaefd49e486ddb8affedfad4e6
Marcel Kraus <git@marcelkraus.de>
2020-02-25 14:36:53 +0100

    Simplify `LoadingIndicatorViewController`

--- Speedometer/Views/Components/LoadingIndicatorViewController.swift
+++ Speedometer/Views/Components/LoadingIndicatorViewController.swift
@@ -3,16 +3,19 @@ import UIKit
 class LoadingIndicatorViewController: UIViewController {
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.alignment = .center
-        stackView.distribution = .fill
         stackView.spacing = 20.0
+        stackView.addArrangedSubview(activityIndicatorView)
+        stackView.addArrangedSubview(indicatorLabel)
 
         return stackView
     }()
 
     private lazy var activityIndicatorView: UIActivityIndicatorView = {
         let activityIndicatorView = UIActivityIndicatorView()
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
         activityIndicatorView.color = .activityIndicator
         activityIndicatorView.startAnimating()
 
@@ -20,35 +23,22 @@ class LoadingIndicatorViewController: UIViewController {
     }()
 
     private lazy var indicatorLabel: UILabel = {
-        let label = UILabel()
-        label.font = .activityIndicator
-        label.textColor = .activityIndicator
-        label.text = "LoadingIndicatorViewController.Indicator".localized
+        let indicatorLabel = UILabel()
+        indicatorLabel.translatesAutoresizingMaskIntoConstraints = false
+        indicatorLabel.font = .activityIndicator
+        indicatorLabel.textColor = .activityIndicator
+        indicatorLabel.text = "LoadingIndicatorViewController.Indicator".localized
 
-        return label
+        return indicatorLabel
     }()
 
-    init() {
-        super.init(nibName: nil, bundle: nil)
-
-        setupStackView()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-private extension LoadingIndicatorViewController {
-    func setupStackView() {
-        stackView.addArrangedSubview(activityIndicatorView)
-        stackView.addArrangedSubview(indicatorLabel)
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
         view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
-            ])
+        ])
     }
 }
