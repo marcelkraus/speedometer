ebd5830f1f3aff62bade70aa5dfc810c9cda909d
Marcel Kraus <git@marcelkraus.de>
2020-03-30 13:41:48 +0200

    Add debug implementation for `paymentQueue(_:updatedTransactions)`

--- Speedometer/Supporting Files/StoreObserver.swift
+++ Speedometer/Supporting Files/StoreObserver.swift
@@ -2,6 +2,21 @@ import StoreKit
 
 class StoreObserver: NSObject, SKPaymentTransactionObserver {
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        print("TODO: Implement observer")
+         for transaction in transactions {
+            switch transaction.transactionState {
+                case .purchasing:
+                    print("purchasing")
+                case .deferred:
+                    print("deferred")
+                case .failed:
+                    print("failed: \(transaction.error)")
+                case .purchased:
+                    print("purchased")
+                case .restored:
+                    print("restored")
+
+         @unknown default: print("Unexpected transaction state \(transaction.transactionState)")
+        }
+         }
     }
 }
